57a6c87ed64b454b2622b22e40b40153700aa1c2
vidaru <vidaru@protonmail.com>
2020-10-09 15:26:37 -0500

    Favicon added

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,9 @@ export default {
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no'}
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
   css: [
