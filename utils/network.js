289b918ac0ba6e62ca75e68f3710564b75da4e40
vidaru <vidaru@protonmail.com>
2020-10-15 09:55:50 -0500

    Fix network settings

--- utils/network.js
+++ utils/network.js
@@ -1,15 +1,15 @@
 export default {
   mainnet: {
-    pubkey: 0x3a,
-    pubkeyhash: 0x3a,
-    scripthash: 0x32,
-    witness_v0_keyhash: 'qc',
-    witness_v0_scripthash: 'qc'
+    pubkey: 0x3f,
+    pubkeyhash: 0x7d,
+    scripthash: 0x80,
+    witness_v0_keyhash: 'sc',
+    witness_v0_scripthash: 'sc'
   },
   testnet: {
-    pubkey: 0x78,
-    pubkeyhash: 0x78,
-    scripthash: 0x6e,
+    pubkey: 0x41,
+    pubkeyhash: 0x7F,
+    scripthash: 0x82,
     witness_v0_keyhash: 'tq',
     witness_v0_scripthash: 'tq'
   }
